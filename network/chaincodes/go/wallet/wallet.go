ec27e8bb3eab95e40507be24f71cb0a76a8dee26
glebpopov <gleb.popov@dataart.com>
2017-10-12 16:31:55 +0300

    refactoring

--- network/chaincodes/go/wallet/wallet.go
+++ network/chaincodes/go/wallet/wallet.go
@@ -1,26 +1,4 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
-
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
 
 import (
 	"fmt"
@@ -60,14 +38,14 @@ func ParseCreatorCertificate(stub shim.ChaincodeStubInterface) (string, error) {
 		return "", errors.New("Error received on pem.Decode of certificate")
 	}
 
-	ucert, err := x509.ParseCertificate(block.Bytes)
+	uCert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		fmt.Printf("Error received on ParseCertificate %s", err)
 		return "", err
 	}
 
-	fmt.Printf("Common Name %s ", ucert.Subject.CommonName)
-	return ucert.Subject.CommonName, nil
+	fmt.Printf("Common Name %s ", uCert.Subject.CommonName)
+	return uCert.Subject.CommonName, nil
 }
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
@@ -97,32 +75,32 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 func (t *SimpleChaincode) create(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A, Cert string
-	A = args[0]
+	var accountName, cert string
+	accountName = args[0]
 
-	Avalbytes, err := stub.GetState(A)
+	accountValBytes, err := stub.GetState(accountName)
 	if err != nil {
 		return shim.Error("Failed to get state")
 	}
-	if Avalbytes != nil {
+	if accountValBytes != nil {
 		return shim.Error("Entity already exists")
 	}
 
-	Cert, err = ParseCreatorCertificate(stub)
+	cert, err = ParseCreatorCertificate(stub)
 
-	err = stub.PutState(A, []byte(strconv.Itoa(100)))
+	err = stub.PutState(accountName, []byte(strconv.Itoa(100)))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	TotalBytes, err := stub.GetState("total_amount")
+	totalBytes, err := stub.GetState("total_amount")
 	if err != nil {
 		return shim.Error("Failed to get state")
 	}
-	if TotalBytes == nil {
+	if totalBytes == nil {
 		return shim.Error("Entity not found")
 	}
-	Total, _ := strconv.Atoi(string(TotalBytes))
+	Total, _ := strconv.Atoi(string(totalBytes))
 
 
 	err = stub.PutState("total_amount", []byte(strconv.Itoa(Total + 100)))
@@ -130,7 +108,7 @@ func (t *SimpleChaincode) create(stub shim.ChaincodeStubInterface, args []string
 		return shim.Error(err.Error())
 	}
 
-	err = stub.PutState(A + "_owner", []byte(Cert))
+	err = stub.PutState(accountName + "_owner", []byte(cert))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -139,62 +117,62 @@ func (t *SimpleChaincode) create(stub shim.ChaincodeStubInterface, args []string
 
 // Transaction makes payment of X units from A to B
 func (t *SimpleChaincode) charge(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A, B, Cert string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
+	var sourceAccount, destinationAccount, cert string    // Entities
+	var sourceValue, destinationValue int // Asset holdings
+	var chargeAmount int          // Transaction value
 	var err error
 
 	if len(args) != 3 {
 		return shim.Error("Incorrect number of arguments. Expecting 3")
 	}
 
-	A = args[0]
-	B = args[1]
+	sourceAccount = args[0]
+	destinationAccount = args[1]
 
-	Cert, err = ParseCreatorCertificate(stub)
+	cert, err = ParseCreatorCertificate(stub)
 
-	Avalcert, err := stub.GetState(A + "_owner")
+	accountCertBytes, err := stub.GetState(sourceAccount + "_owner")
 
-	if string(Avalcert) != Cert {
+	if string(accountCertBytes) != cert {
 		return shim.Error("Invalid certificate")
 	}
 
 	// Get the state from the ledger
 	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
+	sourceValBytes, err := stub.GetState(sourceAccount)
 	if err != nil {
 		return shim.Error("Failed to get state")
 	}
-	if Avalbytes == nil {
+	if sourceValBytes == nil {
 		return shim.Error("Entity not found")
 	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
+	sourceValue, _ = strconv.Atoi(string(sourceValBytes))
 
-	Bvalbytes, err := stub.GetState(B)
+	destinationValBytes, err := stub.GetState(destinationAccount)
 	if err != nil {
 		return shim.Error("Failed to get state")
 	}
-	if Bvalbytes == nil {
+	if destinationValBytes == nil {
 		return shim.Error("Entity not found")
 	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
+	destinationValue, _ = strconv.Atoi(string(destinationValBytes))
 
 	// Perform the execution
-	X, err = strconv.Atoi(args[2])
+	chargeAmount, err = strconv.Atoi(args[2])
 	if err != nil {
 		return shim.Error("Invalid transaction amount, expecting a integer value")
 	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+	sourceValue = sourceValue - chargeAmount
+	destinationValue = destinationValue + chargeAmount
+	fmt.Printf("Aval = %d, Bval = %d\n", sourceValue, destinationValue)
 
 	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	err = stub.PutState(sourceAccount, []byte(strconv.Itoa(sourceValue)))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	err = stub.PutState(destinationAccount, []byte(strconv.Itoa(destinationValue)))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -204,30 +182,30 @@ func (t *SimpleChaincode) charge(stub shim.ChaincodeStubInterface, args []string
 
 // query callback representing the query of a chaincode
 func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A string // Entities
+	var accountName string // Entities
 	var err error
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
 	}
 
-	A = args[0]
+	accountName = args[0]
 
 	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
+	accountValBytes, err := stub.GetState(accountName)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		jsonResp := "{\"Error\":\"Failed to get state for " + accountName + "\"}"
 		return shim.Error(jsonResp)
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+	if accountValBytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + accountName + "\"}"
 		return shim.Error(jsonResp)
 	}
 
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	jsonResp := "{\"Name\":\"" + accountName + "\",\"Amount\":\"" + string(accountValBytes) + "\"}"
 	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
+	return shim.Success(accountValBytes)
 }
 
 func main() {
